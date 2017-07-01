2bf079da2f880604882059019f75538731f84831
hwiewie <hwiewie@gmail.com>
2017-07-01 22:05:56 +0800

    Create zbxtg_settings.py

---
+++ zbxtg_settings.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+tg_key = "XYZ"  # telegram bot api key
+
+zbx_tg_prefix = "zbxtg"  # variable for separating text from script info
+zbx_tg_tmp_dir = "/tmp/" + zbx_tg_prefix  # directory for saving caches, uids, cookies, etc.
+zbx_tg_signature = False
+
+zbx_server = "http://localhost"  # zabbix server full url
+zbx_api_user = "api"
+zbx_api_pass = "api"
+zbx_api_verify = True  # True - do not ignore self signed certificates, False - ignore
+
+proxy_to_zbx = None
+proxy_to_tg = None
+
+#proxy_to_zbx = "proxy.local:3128"
+#proxy_to_tg = "proxy.local:3128"
+
+emoji_map = {
+    "ok": "✅",
+    "problem": "❗",
+    "info": "ℹ️",
+    "warning": "⚠️",
+    "disaster": "❌",
+    "bomb": "💣",
+    "fire": "🔥",
+    "hankey": "💩",
+}
