3423660b06a4b178d8a2b7d112141a8e1f147a46
hwiewie <hwiewie@gmail.com>
2017-07-21 00:54:14 +0800

    Update find_timeout_error_log.sh

--- find_timeout_error_log.sh
+++ find_timeout_error_log.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#取最近一段時間，看error log裡面是否有110 connection timeout事件，撈出事件，顯示時間、來源IP與訪問domain
 cd /opt/logs/nginx/
 anhour=`date '+%H:' -d '-10 minutes'`
 FILESE=*60??.error.log
@@ -11,4 +12,3 @@ do
    n2=`wc -l $fe | awk '{print $1}'`
    sed -n ${n1},${n2}p $fe | grep -n "110: Connection timed out" | awk '{print $2, $17, $27}'
 done
-
