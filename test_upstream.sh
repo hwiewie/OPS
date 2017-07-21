70280c9bc4e22bbe31bd2934cd2520f6394ed5a9
hwiewie <hwiewie@gmail.com>
2017-07-22 02:31:09 +0800

    Update test_upstream.sh

--- test_upstream.sh
+++ test_upstream.sh
@@ -3,5 +3,10 @@ domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print
 upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g'`
 for fe in $upstreams
 do
-   curl -A "Mozilla/5.0 (Linux; Android 4.4.4; Nexus 5 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.114 Mobile Safari/537.36" -v $fe
+   #curl -A "Mozilla/5.0 (Linux; Android 4.4.4; Nexus 5 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.114 Mobile Safari/537.36" -v $fe
+   exec 5<> /dev/tcp/`echo $fe | awk -F':' '{print $1}'`/`echo $fe | awk -F':' '{print $2}'`
+   #cat <&5 &
+   #printf "GET / HTTP/1.1\r\n\r\n" >&5
+   echo -e "GET / HTTP/1.1\r\nhost: http://$fe\r\nConnection: close\r\n\r\n" >&5
+   cat <&5
 done
