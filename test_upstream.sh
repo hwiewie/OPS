6bd6eb9b03a872031332d53d6fe6776088abbeb0
hwiewie <hwiewie@gmail.com>
2017-07-22 23:31:39 +0800

    Update test_upstream.sh

--- test_upstream.sh
+++ test_upstream.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name' | sed -r 's/(.*server_name\s*|;)//g'` | sort -u
 upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g'`
-for fe in $upstreams
 if [! -n "$upstreams"];then
    upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):([0-9]{2,5})' | sed -r 's/(.*server\s*|;)//g'`
 fi
+for fe in $upstreams
 do
    #curl -A "Mozilla/5.0 (Linux; Android 4.4.4; Nexus 5 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.114 Mobile Safari/537.36" -v $fe | awk 'BEGIN{FIELDWIDTHS="5 29"}{gsub(/^[ \t]+/, "", $2); gsub(/[ \t]+$/, "", $2); print $2}'
    exec 5<> /dev/tcp/`echo $fe | awk -F':' '{print $1}'`/`echo $fe | awk -F':' '{print $2}'`
