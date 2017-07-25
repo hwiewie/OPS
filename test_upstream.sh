0f380480ae6f1a59eef94d6518b3c806a0930a6c
hwiewie <hwiewie@gmail.com>
2017-07-25 19:58:58 +0800

    Update test_upstream.sh

--- test_upstream.sh
+++ test_upstream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name' | sed -r 's/(.*server_name\s*|;)//g'` | sort -u
-upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g'`
+domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server_name' | sed -r 's/(.*server_name\s*|;)//g'` | sort -u
+upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g'`
 if [ "$upstreams" = "" ]; then
-   upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):([0-9]{2,5})' | sed -r 's/(.*server\s*|;)//g' | awk '{print $1}'`
+   upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):([0-9]{2,5})' | sed -r 's/(.*server\s*|;)//g' | awk '{print $1}'`
 fi
 for fe in $upstreams
 do
