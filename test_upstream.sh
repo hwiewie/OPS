f37a392faf6b821a0698e0794db5d45f1c02febd
hwiewie <hwiewie@gmail.com>
2017-09-23 19:00:07 +0800

    Update test_upstream.sh

--- test_upstream.sh
+++ test_upstream.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #抓vhost conf內upstream，並發http request，將1.upstream 2.Http status code 3.網頁title 列出
 domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server_name' | sed -r 's/(.*server_name\s*|;)//g'` | sort -u
-upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g'`
+upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):60' | sed -r 's/(.*server\s*|;)//g' | uniq`
 if [ "$upstreams" = "" ]; then
-   upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):([0-9]{2,5})' | sed -r 's/(.*server\s*|;)//g' | awk '{print $1}'`
+   upstreams=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*[^#]server ([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}):([0-9]{2,5})' | sed -r 's/(.*server\s*|;)//g' | awk '{print $1}' | uniq`
 fi
 for fe in $upstreams
 do
