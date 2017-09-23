2fb3da3d8b823994484c3d116206cc0e237fcc3b
hwiewie <hwiewie@gmail.com>
2017-09-23 18:58:02 +0800

    Update vhost_server_name_dnslookup.sh

--- vhost_server_name_dnslookup.sh
+++ vhost_server_name_dnslookup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #抓取vhost config內綁定域名並解析出IP，把每個域名與IP中間用空白隔開，並每筆輸出成一行
 #先在reverse proxy上執行下面指令，並將結果貼在domains=""內
-#find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name' | sed -r 's/(.*server_name\s*|;)//g'
+#find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name' | sed -r 's/(.*server_name\s*|;)//g' | uniq
 domains=""
 regex='^\w+\.+\w'
 for domain in $domains; do
