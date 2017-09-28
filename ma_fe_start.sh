172b22e94c69c92afdc7e177fdbc2c254d0dcde3
hwiewie <hwiewie@gmail.com>
2017-09-28 21:50:58 +0800

    Update ma_fe_start.sh

--- ma_fe_start.sh
+++ ma_fe_start.sh
@@ -1,6 +1,9 @@
 sed -i 's/set $MAM 0/set $MAM 1/g' /opt/APP/openresty/nginx/conf/vhost/*.conf
 
-curl -s https://raw.githubusercontent.com/nickchangs/ma/master/maintain.html -o "/opt/Htdocs/ma/maintain.html"
-curl -s https://raw.githubusercontent.com/nickchangs/ma/master/wap_maintain.html -o "/opt/Htdocs/ma/wap_maintain.html"
+sed -i '/维护時間/d' /opt/Htdocs/ma/maintain.html
+sed -i '/维护時間/d' /opt/Htdocs/ma/wap_maintain.html
+
+sed -i '/在线客服/i/\<p\>\<span\ class\=\"red\"\>维护時間 $1\-$2\<\/span\>\<\/p\>' /opt/Htdocs/ma/maintain.html
+sed -i '/在线客服/i/\<p\>\<span\ class\=\"red\"\>维护時間 $1\-$2\<\/span\>\<\/p\>' /opt/Htdocs/ma/wap_maintain.htm
 
 nginx -s reload
