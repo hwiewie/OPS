70704bba38feffcbc0ee138c690551752c7a6b4a
hwiewie <hwiewie@gmail.com>
2017-09-28 17:31:42 +0800

    Create ma_app_start.sh

---
+++ ma_app_start.sh
@@ -0,0 +1,5 @@
+sed -i 's/include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/#include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
+sed -i 's/#include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
+
+curl -s https://raw.githubusercontent.com/nickchangs/ma/master/ma.conf -o "/opt/APP/openresty/nginx/conf/ma/ma.conf"
+nginx -s reload
