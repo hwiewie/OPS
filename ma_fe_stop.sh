4f2ed340fdf8588de9b3fa1f966499d401c9ae0c
hwiewie <hwiewie@gmail.com>
2017-09-29 00:06:23 +0800

    Update ma_fe_stop.sh

--- ma_fe_stop.sh
+++ ma_fe_stop.sh
@@ -2,3 +2,8 @@ sed -i 's/conf\/ma/conf\/vhost/g' /opt/APP/openresty/nginx/conf/nginx.conf
 sed -i '/维护時間/d' /opt/Htdocs/ma/maintain.html
 sed -i '/维护時間/d' /opt/Htdocs/ma/wap_maintain.html
 nginx -s reload
+if [ $? = 0 ]; then
+echo "成功"
+else
+echo "失敗"
+fi
