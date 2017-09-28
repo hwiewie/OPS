b6ce905acb94221e7f4de8b7afb33721fc77b8c2
hwiewie <hwiewie@gmail.com>
2017-09-28 17:41:18 +0800

    Update ma_app_start.sh

--- ma_app_start.sh
+++ ma_app_start.sh
@@ -1,5 +1,5 @@
 sed -i 's/include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/#include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
 sed -i 's/#include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
 
-curl -s https://raw.githubusercontent.com/nickchangs/ma/master/ma.conf -o "/opt/APP/openresty/nginx/conf/ma/ma.conf"
+curl -s https://raw.githubusercontent.com/nickchangs/ma/master/ma-APP.conf -o "/opt/APP/openresty/nginx/conf/ma/ma.conf"
 nginx -s reload
