72f655b601a32f31e1e2edfcfe14a8e5bd1c2a78
hwiewie <hwiewie@gmail.com>
2017-09-29 02:54:13 +0800

    Update ma_app_start.sh

--- ma_app_start.sh
+++ ma_app_start.sh
@@ -3,9 +3,15 @@ ttoday=`date +%m-%d`
 echo "把nginx.conf內include vhost下所有conf改成include ma下所有conf"
 sed -i 's/include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/\#include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
 if [ $? = 0 ]; then
-    echo "切換ma目錄成功"
+    echo "註解vhost成功"
 else
-    echo "切換ma目錄失敗"
+    echo "註解vhost失敗"
+fi
+sed -i 's/\#include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
+if [ $? = 0 ]; then
+    echo "載入ma.conf成功"
+else
+    echo "載入ma.conf失敗"
 fi
 echo "修改ma.conf，設定開始維護與停止維護時間"
 echo "維護開始時間：{$1}，維護停止時間：{$2}"
