a0664344994b17e6ebf0f103c61f579e46c29598
hwiewie <hwiewie@gmail.com>
2017-09-29 00:51:56 +0800

    Update ma_app_start.sh

--- ma_app_start.sh
+++ ma_app_start.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+ttoday=`date +%m-%d`
+echo "把nginx.conf內include vhost下所有conf改成include ma下所有conf"
 sed -i 's/include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/\#include \/opt\/APP\/openresty\/nginx\/conf\/vhost\/*.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
-sed -i 's/\#include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/include \/opt\/APP\/openresty\/nginx\/conf\/ma\/ma.conf;/g' /opt/APP/openresty/nginx/conf/nginx.conf
+if [ $? = 0 ]; then
+    echo "切換ma目錄成功"
+else
+    echo "切換ma目錄失敗"
+fi
+echo "修改ma.conf，設定開始維護與停止維護時間"
+echo "維護開始時間：{$1}，維護停止時間：{$2}"
 
-curl -s https://raw.githubusercontent.com/nickchangs/ma/master/ma-APP.conf -o "/opt/APP/openresty/nginx/conf/ma/ma.conf"
+echo "重新載入nginx設定檔"
 nginx -s reload
+if [ $? = 0 ]; then
+    echo "重新載入nginx設定檔成功"
+    echo "完成！請輸入該品牌app主網域網址進行測試！"
+else
+    echo "切換ma目錄失敗"
+    echo "失敗！請確認/opt/APP/openresty/nginx/conf/ma/ma.conf內容是否正確！"
+fi
