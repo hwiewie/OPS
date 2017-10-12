fcdacc8d6e63470aecd78e511394a52e221fca64
hwiewie <hwiewie@gmail.com>
2017-10-12 21:33:12 +0800

    Update geoip_add.sh

--- geoip_add.sh
+++ geoip_add.sh
@@ -10,24 +10,27 @@ fi
 #新增geoip
 if [ $1 == add ]; then
     sed -i '/^\s*default\ 1/a\          '$2'\/32\ 0;' /opt/APP/openresty/nginx/conf/nginx.conf
-if [ $? = 0 ]; then
-    echo "新增geoip白名單成功"
-else
-    echo "新增geoip白名單失敗"
-    exit
-#重啟nginx服務
-nginx -t
-if [ $? = 0 ]; then
-    echo "測試nginx設定檔成功，開始重啟nginx"
-	service nginx restart
-	if [ $? = 0 ]; then
-	    echo "重新啟動nginx設定檔成功"
-	else
-	    echo "重啟nginx失敗，請使用nginx -t查詢正確認"
-		exit
+    if [ $? = 0 ]; then
+        echo "新增geoip白名單成功"
+		#重啟nginx服務
+		nginx -t
+		if [ $? = 0 ]; then
+		    echo "測試nginx設定檔成功，開始重啟nginx"
+			service nginx restart
+			if [ $? = 0 ]; then
+			    echo "重新啟動nginx設定檔成功"
+			else
+			    echo "重啟nginx失敗，請使用nginx -t查詢正確認"
+				exit
+			fi
+		else
+		    echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
+		    exit
+		fi
+    else
+        echo "新增geoip白名單失敗"
+        exit
 	fi
-else
-    echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
 fi
 #刪除geoip
 if [ $1 == del ]; then
