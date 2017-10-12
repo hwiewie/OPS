12707f3b61df4a24214fc2b8f8c6128ed149e4dc
hwiewie <hwiewie@gmail.com>
2017-10-12 22:06:09 +0800

    Update geoip_add.sh

--- geoip_add.sh
+++ geoip_add.sh
@@ -12,49 +12,49 @@ if [ $1 == add ]; then
     sed -i "/^\s*default\ 1/a\          $2\/32\ 0;" $filepathe
     if [ $? = 0 ]; then
         echo "新增geoip白名單成功"
-		#重啟nginx服務
-		nginx -t
-		if [ $? = 0 ]; then
-		    echo "測試nginx設定檔成功，開始重啟nginx"
-			service nginx restart
-			if [ $? = 0 ]; then
-			    echo "重新啟動nginx設定檔成功"
-			else
-			    echo "重啟nginx失敗，請使用nginx -t查詢正確認"
-				exit
-			fi
-		else
-		    echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
-		    exit
-		fi
+        #重啟nginx服務
+        nginx -t
+        if [ $? = 0 ]; then
+            echo "測試nginx設定檔成功，開始重啟nginx"
+            service nginx restart
+            if [ $? = 0 ]; then
+                echo "重新啟動nginx設定檔成功"
+            else
+                echo "重啟nginx失敗，請使用nginx -t查詢正確認"
+                exit
+            fi
+        else
+            echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
+            exit
+        fi
     else
         echo "新增geoip白名單失敗"
         exit
-	fi
+    fi
 fi
 #刪除geoip
 if [ $1 == del ]; then
     sed -i "/^\s*$2/d" $filepathe
     if [ $? = 0 ]; then
         echo "刪除geoip白名單成功"
-		#重啟nginx服務
+        #重啟nginx服務
         nginx -t
-		if [ $? = 0 ]; then
-		    echo "測試nginx設定檔成功，開始重啟nginx"
-			service nginx restart
-			if [ $? = 0 ]; then
-			    echo "重新啟動nginx設定檔成功"
-			else
-			    echo "重啟nginx失敗，請使用nginx -t查詢正確認"
-				exit
-			fi
-		else
-		    echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
-		fi
+        if [ $? = 0 ]; then
+            echo "測試nginx設定檔成功，開始重啟nginx"
+            service nginx restart
+            if [ $? = 0 ]; then
+                echo "重新啟動nginx設定檔成功"
+            else
+                echo "重啟nginx失敗，請使用nginx -t查詢正確認"
+                exit
+            fi
+        else
+            echo "測試nginx設定檔失敗，請查看/opt/APP/openresty/nginx/conf/nginx.conf確認那邊出錯"
+        fi
     else
         echo "刪除geoip白名單失敗"
         exit
-	fi
+    fi
 fi
 #顯示geoip清單
 if [ $1 == show ]; then
