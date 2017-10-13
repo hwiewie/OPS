6d7e6545e8cfcdc24f4a2c0fc936f769b3b5878b
hwiewie <hwiewie@gmail.com>
2017-10-14 01:30:25 +0800

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -34,11 +34,23 @@ wget http://ftp.otrs.org/pub/otrs/RPMS/rhel/7/otrs-6.0.0.beta3-01.noarch.rpm
 yum install check otrs-6.0.0.beta3-01.noarch.rpm
 yum install "perl(Text::CSV_XS)" "perl(Crypt::Eksblowfish::Bcrypt)" "perl(YAML::XS)" "perl(JSON::XS)" "perl(Encode::HanExtra)" "perl(Mail::IMAPClient)" "perl(ModPerl::Util)" "perl(DBD::Pg)" "perl(Authen::NTLM)"
 /opt/otrs/bin/otrs.CheckModules.pl
+mysql -u root -p
+create database otrs character set utf8 collate utf8_bin ;
+grant all privileges on otrs.* to otrs@localhost identified by "some-pass";
+flush privileges;
+quit;
+mysql -u root -p otrs < /opt/otrs/scripts/database/otrs-schema.mysql.sql
+systemctl restart mariadb
+systemctl restart httpd
+http://172.16.100.134/otrs/installer.pl
 
 echo "安裝gitlab"
 yum install git
+git config --global user.name "Your Name"
+git config --global user.email "you@example.com"
 firewall-cmd --permanent --add-service=http
 systemctl reload firewalld
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
 yum install gitlab-ce
 gitlab-ctl reconfigure
+
