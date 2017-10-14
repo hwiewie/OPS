d041733b84b432f2e336d19f04d65a01f7c53fa5
hwiewie <hwiewie@gmail.com>
2017-10-14 23:46:51 +0800

    Update iptables_add.sh

--- iptables_add.sh
+++ iptables_add.sh
@@ -10,77 +10,138 @@ else
     exit
 fi
 #判斷輸入參數個數
-case "$#" in
-"1")
-    echo "1"
-	grep "^[- ].*$1" $filepathe > /dev/null
-	if [ $? = 1 ] ;then
-	    echo "開始將IP：$1 加入白名單"
-        #看是否被註解
-        grep "^#.*$1" $filepathe > /dev/null
-		if [ $? = 0 ] ;then
-            #移掉註解
-            echo "移掉註解"
-			sed -i 's/^#//g' $filepathe
+if [ $# = 0 ];then
+    echo "參數錯誤："
+    exit
+fi
+#判斷是新增還是刪除
+case "$1" in
+"add")
+    #判斷輸入參數個數
+    case "$#" in
+    "2")
+	    echo "只輸入IP"
+	    grep "^[- ].*$2" $filepathe > /dev/null
+	    if [ $? = 1 ] ;then
+	        echo "開始將IP：$2 加入白名單"
+            #看是否被註解
+            grep "^#.*$2" $filepathe > /dev/null
+		    if [ $? = 0 ] ;then
+                #移掉註解
+                echo "移掉註解"
+			    sed -i 's/^#//g' $filepathe
+            else
+                #裡面沒這筆IP記錄，開始新增
+                echo "使用iptables -A指令新增"
+                iptables -A INPUT -s $2/32 -p tcp -m multiport --dports 443,80 -m comment --comment "$ttoday Bot used Apply" -j ACCEPT
+            fi	     
+            #如果移掉註解或新增記錄成功，就重載iptables		
+            if [ $? = 0 ] ;then
+                echo "開始重新載入iptables"
+                systemctl reload iptables
+                if [ $? = 0 ] ;then
+                    echo "iptables重載成功"
+                else
+                    echo "iptables重載失敗"
+                    exit
+                fi
+            else
+                echo "加入白名單失敗"
+                exit
+            fi
         else
+            echo "已加入過白名單了"
+		    exit
+        fi
+	    ;;
+    "3")
+        echo "2"
+	    grep "^[- ].*$2" $filepathe > /dev/null
+	    if [ $? = 1 ] ;then
+	        echo "開始將IP：$2 加入白名單"
+            #看是否被註解
+            grep "^#.*$2" $filepathe > /dev/null
+		    if [ $? = 0 ] ;then
+                #刪除註解
+                echo "移掉舊記錄"
+			    sed -i '/^#.*'$2'//g' $filepathe
+            fi
             #裡面沒這筆IP記錄，開始新增
             echo "使用iptables -A指令新增"
-            iptables -A INPUT -s $1/32 -p tcp -m multiport --dports 443,80 -m comment -- comment "$ttoday Bot used Apply" -j ACCEPT
-        fi	     
-        #如果移掉註解或新增記錄成功，就重載iptables		
-        if [ $? = 0 ] ;then
-            echo "開始重新載入iptables"
-            systemctl reload iptables
-            if [ $? = 0 ] ;then
-                echo "iptables重載成功"
+            iptables -A INPUT -s $2/32 -p tcp -m multiport --dports 443,80 -m comment --comment "$ttoday $3 used Apply" -j ACCEPT	     
+            #如果移掉註解或新增記錄成功，就重載iptables		
+		    if [ $? = 0 ] ;then
+                echo "開始重新載入iptables"
+                systemctl reload iptables
+                if [ $? = 0 ] ;then
+                    echo "iptables重載成功"
+                else
+                    echo "iptables重載失敗"
+                    exit
+                fi
             else
-                echo "iptables重載失敗"
-                exit
-            fi
+		        echo "加入白名單失敗"
+			    exit
+		    fi
         else
-            echo "加入白名單失敗"
-            exit
+            echo "已加入過白名單了"
+		    exit
         fi
-    else
-        echo "已加入過白名單了"
-		exit
-    fi
-	;;
-"2")
-    echo "2"
-	grep "^[- ].*$1" $filepathe > /dev/null
-	if [ $? = 1 ] ;then
-	    echo "開始將IP：$1 加入白名單"
-        #看是否被註解
-        grep "^#.*$1" $filepathe > /dev/null
-		if [ $? = 0 ] ;then
-            #刪除註解
-            echo "移掉舊記錄"
-			sed -i '/^#.*'$1'//g' $filepathe
-        fi
-        #裡面沒這筆IP記錄，開始新增
-        echo "使用iptables -A指令新增"
-        iptables -A INPUT -s $1/32 -p tcp -m multiport --dports 443,80 -m comment -- comment "$ttoday $2 used Apply" -j ACCEPT	     
-        #如果移掉註解或新增記錄成功，就重載iptables		
-		if [ $? = 0 ] ;then
-            echo "開始重新載入iptables"
-            systemctl reload iptables
-            if [ $? = 0 ] ;then
-                echo "iptables重載成功"
-            else
-                echo "iptables重載失敗"
-                exit
-            fi
-        else
-		    echo "加入白名單失敗"
+	    ;;
+    *)
+        echo "Parameter input error"
+	    ;;
+    esac
+    ;;
+"del")
+    #找那筆記錄
+    grep "^[- ].*$2" $filepathe > /dev/null
+	if [ $? = 0 ] ;then
+	    sed -i 's/^\-.*'$2'/#&/g/' $filepathe
+	    if [ $? = 0 ] ;then
+	        echo "已將此筆記錄註解"
+	    else
+		    echo "註解失敗"
 			exit
 		fi
-    else
-        echo "已加入過白名單了"
+	else
+	    echo "找不到記錄"
 		exit
+	fi
+	echo "開始重新載入iptables"
+    systemctl reload iptables
+    if [ $? = 0 ] ;then
+        echo "iptables重載成功"
+    else
+        echo "iptables重載失敗"
+        exit
     fi
+    ;;
+"show")
+    #判斷輸入參數個數
+    case "$#" in
+	"1")
+        grep '^\-.*[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $filepathe
+	    ;;
+	"2")
+        grep "$2" $filepathe
+		;;
+	*)
+	    echo "請不要輸入超過3個參數！"
+		;;
+	esac
+	;;
+"help")
+    echo "說明："
+	echo "使用語法：sh test.sh 指令 (IP) (申請者) ，上面括號內參數為選填，視情況使用"
+	echo "指令有： add (新增)  del (刪除)  show (顯示記錄)"
+	echo "IP：就是要綁定或解除的IP"
+	echo "申請者：就是說要做的客服"
+	echo "add與del時一定要輸入IP！"
+	echo "只有add時申請者的參數有用到"
+	echo "show的時候不帶IP就是看全部"
 	;;
 *)
-    echo "Parameter input error"
-	;;
+    echo "第一個參數為必須參數，只有add、del、show及help可用！"
+    ;;
 esac
