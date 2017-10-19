3628cb5e78207229b620395c6ef593d8b3106fc5
hwiewie <hwiewie@gmail.com>
2017-10-19 21:52:34 +0800

    Update upgrade_geoip.sh

--- upgrade_geoip.sh
+++ upgrade_geoip.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+#取得GeoIP.dat路徑
 geoipdata=`grep GeoIP.dat /opt/APP/openresty/nginx/conf/nginx.conf | awk '{print $2}' | sed -r 's/;//g'`
-wget -O $geoipdata https://raw.githubusercontent.com/hwiewie/OPS/master/GeoIP-106_20170926.dat
-nginx -s reload
+#檢查是否有正常抓到GeoIP.dat的路徑
+if [ -e $geoipdata ];then
+    echo "geoip.dat存在，將繼續執行"
+else
+    echo "geoip.dat不存在！將離開程式！"
+    exit
+fi
+#檢查/root/geoip.dat是否存在
+if [ -e /root/geoip ];then
+    echo "有找到新的geoip.dat，將繼續執行"
+else
+    echo "找不到新的geoip.dat！將離開程式！"
+    exit
+fi
+#更新GeoIP.dat
+cp /root/geoip.dat $geoipdata
+#重載nginx設定
+#nginx -s reload
+systemctl reload nginx
