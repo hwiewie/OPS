6a4fdc9c21756973ca51bf3eb2fa7a5af85db717
hwiewie <hwiewie@gmail.com>
2017-11-23 21:03:36 +0800

    Update vhost_server_name_parser.sh

--- vhost_server_name_parser.sh
+++ vhost_server_name_parser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #抓取vhost config內綁定域名並解析出IP，把每個域名與IP中間用空白隔開，並每筆輸出成一行
 regex='^\w+\.+\w'
-domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name[ \s\t]*[0-9A-Za-z]*\.[0-9A-Za-z]*' | sed -r 's/(.*server_name\s*|;)//g' | uniq`
+domains=`find /opt/APP/openresty/nginx/conf/vhost/ -type f -name "*.conf" -print0 | xargs -0 egrep '^(\s|\t)*server_name[ \s\t]*\w+\.+\w' | sed -r 's/(.*server_name\s*|;)//g' | uniq`
 for domain in $domains; do
     if [[ $domain =~ $regex ]]; then
         echo -n $domain;
