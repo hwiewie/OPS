f9d9e0a13c9e2d877081855429b6c6572b146bd0
hwiewie <hwiewie@gmail.com>
2018-03-28 22:08:19 +0800

    Update and rename ELS-install.sh to ELK-install.sh

--- ELK-install.sh
+++ ELK-install.sh
@@ -47,7 +47,7 @@ yum -y install telnet bind-utils net-tools wget nc
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
 rpm -ivh jdk-8u162-linux-x64.rpm
 java -version
-#安裝ELK
+#安裝Elasticsearch
 yum install elasticsearch
 #修改設定
 sed -i 's/-Xmx1g/-Xmx8g/g' /etc/elasticsearch/jvm.options
