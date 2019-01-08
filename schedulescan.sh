e60f75e40aa51ef030fdb889f943e2f17577ca7f
hwiewie <hwiewie@gmail.com>
2019-01-08 11:02:04 +0800

    Update schedulescan.sh

--- schedulescan.sh
+++ schedulescan.sh
@@ -2,6 +2,7 @@
 #scan ip list : all.txt
 #scan repot : result.xml
 #convert to csv : report.csv
+#crontab -e : 20 9 * * 1 schedulescan.sh
 cd /root/portscan
 rm result.xml
 nmap -p- -T4 -iL all.txt -oX result.xml
