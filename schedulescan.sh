33a4b131f0e8d63ac985f8d9dc1165bfa8548715
hwiewie <hwiewie@gmail.com>
2019-01-08 10:54:48 +0800

    Create schedulescan.sh

---
+++ schedulescan.sh
@@ -0,0 +1,9 @@
+#https://raw.githubusercontent.com/laconicwolf/Nmap-Scan-to-CSV/master/nmap_xml_parser.py
+#scan ip list : all.txt
+#scan repot : result.xml
+#convert to csv : report.csv
+cd /root/portscan
+rm result.xml
+nmap -p- -T4 -iL all.txt -oX result.xml
+rm report.csv
+python nmap_xml_parser.py -f result.xml -csv report.csv
