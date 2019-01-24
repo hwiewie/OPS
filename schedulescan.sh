002d4de34fed8a48a7962757bb370fe59cf6273f
hwiewie <hwiewie@gmail.com>
2019-01-24 10:50:39 +0800

    Update schedulescan.sh

--- schedulescan.sh
+++ schedulescan.sh
@@ -4,7 +4,11 @@
 #convert to csv : report.csv
 #crontab -e : 20 9 * * 1 schedulescan.sh
 cd /root/portscan
-rm result.xml
-nmap -p- -T4 -iL all.txt -oX result.xml
+rm -rf discovery.gnmap
+nmap -sn -T4 -iL all.txt -oG discovery.gnmap
+rm -rf livehost.txt
+grep "Status: Up" discovery.gnmap | cut -f 2 -d" " > livehost.txt
+rm -rf result.xml
+nmap -p- -T4 -iL livehost.txt -oX result.xml
 rm report.csv
 python nmap-parser-xml-to-csv.py result.xml -s , -o report.csv
