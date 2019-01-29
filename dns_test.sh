99fc7bfdda8cc753b7975fbfafae852e863be5a8
hwiewie <hwiewie@gmail.com>
2019-01-29 14:59:18 +0800

    Update dns_test.sh

--- dns_test.sh
+++ dns_test.sh
@@ -8,8 +8,8 @@ if [[ ! -n $1 ]]; then
 else
    testdomain=$1
 fi
-
+#列出可查詢DNS查詢該域名所花費時間
 for fn in $dnsservers; do
-  speeds=`host -a -t a google.com $fn | awk '/^Received / {print $7}'`
-  echo "DNS $fn query time: $speeds"
+  speeds=`host -a -t a $testdomain $fn | awk '/^Received / {print $7}'`
+  echo "DNS $fn query time: $speeds ms"
 done
