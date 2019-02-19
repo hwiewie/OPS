fea8c840f796d9685e0eb355481c76fac037a113
hwiewie <hwiewie@gmail.com>
2019-02-19 11:50:01 +0800

    Update omp.sh

--- omp.sh
+++ omp.sh
@@ -4,3 +4,5 @@
 omp -u admin -w password -T | awk '/T*-IP$/ {print $1}'
 #取得TaskID
 omp -u admin -w password -G | awk '/T*-Weekly-Scan$/ {print $1}'
+#取得ReportID
+omp -u admin -w password -G 72b3cb8f-f044-41d5-8dce-cfbfaefff537 | awk '/T*-Weekly-Scan/{getline; print}' | awk '/ Done / {print $1}'
