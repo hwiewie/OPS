7098facc3e304e59abfbd76f9d0a92bac573b826
hwiewie <hwiewie@gmail.com>
2019-02-27 11:34:23 +0800

    Create getport.py

---
+++ getport.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import sys
+import os
+import unicodecsv as csv
+import codecs
+import numpy as np
+import pandas as pd
+
+def getport(csvFile):
+	df=pd.DataFrame(pd.read_csv(csvFile,header=0))
+	ports=df['port'].unique()
+	np.savetxt(r'port.txt',ports,fmt='%d')
+
+if __name__ == "__main__":
+	csvFile='report.csv'
+	if os.path.exists("port.txt"):
+		os.remove("port.txt")
+    getport(csvFile)
