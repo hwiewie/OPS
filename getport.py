449fd98a8c5d786cb4d79e5c79def3dbe6642192
hwiewie <hwiewie@gmail.com>
2019-02-27 11:38:32 +0800

    Update getport.py

--- getport.py
+++ getport.py
@@ -8,12 +8,12 @@ import numpy as np
 import pandas as pd
 
 def getport(csvFile):
-	df=pd.DataFrame(pd.read_csv(csvFile,header=0))
-	ports=df['port'].unique()
-	np.savetxt(r'port.txt',ports,fmt='%d')
+    df=pd.DataFrame(pd.read_csv(csvFile,header=0))
+    ports=df['port'].sort_values().unique()
+    np.savetxt(r'port.txt',ports,fmt='%d')
 
 if __name__ == "__main__":
-	csvFile='report.csv'
-	if os.path.exists("port.txt"):
-		os.remove("port.txt")
+    csvFile='report.csv'
+    if os.path.exists("port.txt"):
+        os.remove("port.txt")
     getport(csvFile)
