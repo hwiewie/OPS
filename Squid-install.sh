dc78a4879a081cb1109482cf1c85f44250b5d3d7
hwiewie <hwiewie@gmail.com>
2019-03-29 16:23:05 +0800

    Create Squid-install.sh

---
+++ Squid-install.sh
@@ -0,0 +1,12 @@
+yum update
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sed -i 's/server 0.centos.pool.ntp.org iburst/server tock.stdtime.gov.tw iburst/g' /etc/chrony.conf
+sed -i 's/server 1.centos.pool.ntp.org iburst/server watch.stdtime.gov.tw iburst/g' /etc/chrony.conf
+sed -i 's/server 2.centos.pool.ntp.org iburst/server tick.stdtime.gov.tw iburst/g' /etc/chrony.conf
+sed -i 's/server 3.centos.pool.ntp.org iburst/server clock.stdtime.gov.tw iburst/g' /etc/chrony.conf
+sed -i '/watch.stdtime.gov.tw/a server time.stdtime.gov.tw iburst' /etc/chrony.conf
+systemctl restart chronyd
+chronyc -a makestep
+yum -y install epel-release
+yum -y install vim yum-utils telnet bind-utils net-tools wget perl perl-core gcc bzip2 git net-snmp* libcurl-devel
