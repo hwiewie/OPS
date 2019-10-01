b375de50b3f2ec68369155f10c664c2a3d0f1b92
hwiewie <hwiewie@gmail.com>
2019-10-01 10:43:37 +0800

    Update ELK-install.sh

--- ELK-install.sh
+++ ELK-install.sh
@@ -44,8 +44,9 @@ yum -y install epel-release
 yum -y update
 yum -y install telnet bind-utils net-tools wget nc
 #安裝Java(1.8版以上)
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.rpm"
-rpm -ivh jdk-8u202-linux-x64.rpm
+#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.rpm"
+#rpm -ivh jdk-8u202-linux-x64.rpm
+yum install -y java
 java -version
 #安裝Elasticsearch
 yum install -y elasticsearch
