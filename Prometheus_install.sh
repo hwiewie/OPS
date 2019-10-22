f701b4e84b195ecb51668cc3ff1c5dd7d5510ebd
hwiewie <hwiewie@gmail.com>
2019-10-22 14:40:01 +0800

    Update Prometheus_install.sh

--- Prometheus_install.sh
+++ Prometheus_install.sh
@@ -6,3 +6,8 @@ tar -zxvf prometheus-$VERSION.linux-amd64.tar.gz
 mv prometheus-$VERSION.linux-amd64/ /srv/prometheus
 mkdir -pv /srv/prometheus/data
 chown -R prometheus.prometheus /srv/prometheus
+curl -OL https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+tar -zxvf node_exporter-0.18.1.linux-amd64.tar.gz
+mv node_exporter-0.18.1.linux-amd64 /srv/node_exporter
+cp /srv/node_exporter/node_exporter /usr/local/bin/
+node_exporter
