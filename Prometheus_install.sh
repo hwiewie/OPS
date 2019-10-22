28150a0d52c88737b72d62f4611a39420e67feea
hwiewie <hwiewie@gmail.com>
2019-10-22 14:21:05 +0800

    Update Prometheus_install.sh

--- Prometheus_install.sh
+++ Prometheus_install.sh
@@ -1,2 +1,8 @@
 groupadd prometheus
 useradd -g prometheus -M -s /sbin/nologin prometheus
+export VERSION=2.13.1
+wget https://github.com/prometheus/prometheus/releases/download/v$VERSION/prometheus-$VERSION.linux-amd64.tar.gz
+tar -zxvf prometheus-$VERSION.linux-amd64.tar.gz
+mv prometheus-$VERSION.linux-amd64/ /srv/prometheus
+mkdir -pv /srv/prometheus/data
+chown -R prometheus.prometheus /srv/prometheus
