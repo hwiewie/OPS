692d52b06ef551168c887d5002bdc10c048cbff7
hwiewie <hwiewie@gmail.com>
2019-12-10 16:08:43 +0800

    Update add_while_list.sh

--- add_while_list.sh
+++ add_while_list.sh
@@ -1,9 +1,10 @@
+filename=`date +%Y%m%d`
 mv today.txt old.txt
 mv total.txt today.txt
-//cat news.txt today.txt today.txt | sort | uniq -u
+#cat news.txt today.txt today.txt | sort | uniq -u
 cat today.txt news.txt | sort | uniq > total.txt
 cat total.txt | ./cidr.py > result.txt
 cat today.txt | ./cidr.py > temp.txt 
 cat result.txt temp.txt | sort | uniq -u  > aaa.txt
-//cat result.txt temp.txt temp.txt| sort | uniq -u
+#cat result.txt temp.txt temp.txt| sort | uniq -u
 awk BEGIN{RS=EOF}'{gsub(/\n/," or src net ");print}' aaa.txt
