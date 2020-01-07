6aa67cc83e5abba05dcdd6dabd381434076f5e7b
hwiewie <hwiewie@gmail.com>
2020-01-07 10:44:19 +0800

    Update docker_openvas.sh

--- docker_openvas.sh
+++ docker_openvas.sh
@@ -1,3 +1,10 @@
+#install docker
+#yum install -y yum-utils device-mapper-persistent-data lvm2
+#yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#yum-config-manager --enable docker-ce-nightly
+#yum install docker-ce docker-ce-cli containerd.io
+#yum install /path/to/package.rpm
+#systemctl start docker
 docker volume create openvas
 docker run -d -p 443:443 -p 9390:9390 -v openvas:/var/lib/openvas/mgr -e OV_UPDATE=yes --name openvas atomicorp/openvas
 #docker exec -it openvas bash
