af14791e21284b69484e015c47da79a87c65e0e8
hwiewie <hwiewie@gmail.com>
2020-01-13 16:05:22 +0800

    Update docker_openvas.sh

--- docker_openvas.sh
+++ docker_openvas.sh
@@ -2,6 +2,7 @@
 #yum install -y yum-utils device-mapper-persistent-data lvm2
 #yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 #yum-config-manager --enable docker-ce-nightly
+##yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-3.el7.noarch.rpm
 #yum install -y docker-ce docker-ce-cli containerd.io
 #yum install -y /path/to/package.rpm
 #rpm -vhU https://nmap.org/dist/nmap-7.80-1.x86_64.rpm
