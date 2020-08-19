a4349e5cb2413ed2fb2e3829a2145b918c5eca40
hwiewie <hwiewie@gmail.com>
2020-08-19 23:28:46 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,5 @@
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 dnf install docker-ce --nobest -y
 systemctl enable docker --now
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
