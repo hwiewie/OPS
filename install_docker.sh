24696400a183473d69ad39bf0fc43a636a53282e
hwiewie <hwiewie@gmail.com>
2020-08-19 23:57:58 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,3 +3,5 @@ dnf install docker-ce --nobest -y
 systemctl enable docker --now
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+sysctl -p
