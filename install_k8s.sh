aa9d3ccc710bdfbd74144962ea8955a0b5ea9432
hwiewie <hwiewie@gmail.com>
2020-08-20 23:44:25 +0800

    Update install_k8s.sh

--- install_k8s.sh
+++ install_k8s.sh
@@ -2,3 +2,13 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 #kubectl version --client
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
