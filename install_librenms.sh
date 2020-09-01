d6547a0d498c3e1b55086453c23e80cdb26b2e7b
hwiewie <hwiewie@gmail.com>
2020-09-01 23:36:49 +0800

    Update install_librenms.sh

--- install_librenms.sh
+++ install_librenms.sh
@@ -1,4 +1,5 @@
 yum install -y epel-release
+yum install -y git
 yum install -y yum-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 yum module reset php -y
 yum module enable php:remi-7.4 -y
