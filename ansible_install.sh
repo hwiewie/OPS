6bd0ab341e767388bf576672ae54c06554271252
hwiewie <hwiewie@gmail.com>
2020-10-31 14:10:58 +0800

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,6 +1,6 @@
 yum -y install epel-release python38
 yum -y update
-pip install --upgrade pip
-yum -y install ansible
+#pip install --upgrade pip
+pip3 install ansible
 ansible --version
 pip install ansible-lint
