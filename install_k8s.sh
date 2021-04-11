90ee648abff98f702d920081e27ac2b2a8f6f110
hwiewie <hwiewie@gmail.com>
2021-04-11 10:51:41 +0800

    Update install_k8s.sh

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,3 +1,12 @@
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
